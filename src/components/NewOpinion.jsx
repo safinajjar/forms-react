fea98a19a32257e1738fceee7d1c99884a405f98
Musti <safi.najjar1@gmail.com>
2025-02-23 18:24:22 +0100

    initialize project with Vite, React, and basic structure for opinion sharing app - focus on hanlding forms with form actions

---
+++ src/components/NewOpinion.jsx
@@ -0,0 +1,90 @@
+import { useActionState } from "react";
+
+export function NewOpinion() {
+  const shareOpinionAction = (_prevState, formData) => {
+    const title = formData.get("title");
+    const userName = formData.get("userName");
+    const body = formData.get("body");
+
+    let errors = [];
+
+    if (title.trim().length < 5) {
+      errors.push("Title must be at least 5 characters long.");
+    }
+
+    if (!userName) {
+      errors.push("User name is required.");
+    }
+
+    if (body.trim().length < 10 || body.trim().length > 300) {
+      errors.push("Opinion body must be between 10 and 300 characters long.");
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: { title, userName, body },
+      };
+    }
+
+    // submit to backend
+
+    return {
+      errors: [],
+    };
+  };
+
+  const [formState, formAction] = useActionState(shareOpinionAction, {
+    errors: null,
+  });
+
+  return (
+    <div id="new-opinion">
+      <h2>Share your opinion!</h2>
+      <form action={formAction}>
+        <div className="control-row">
+          <p className="control">
+            <label htmlFor="userName">Your Name</label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              defaultValue={formState.enteredValues?.userName}
+            />
+          </p>
+
+          <p className="control">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              defaultChecked={formState.enteredValues?.title}
+            />
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="body">Your Opinion</label>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultChecked={formState.enteredValues?.body}
+          ></textarea>
+        </p>
+
+        {formState.errors && (
+          <ul className="errors">
+            {formState.errors.map((error) => {
+              return <li key={error}>{error}</li>;
+            })}
+          </ul>
+        )}
+
+        <p className="actions">
+          <button type="submit">Submit</button>
+        </p>
+      </form>
+    </div>
+  );
+}
